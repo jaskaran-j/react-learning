9661cecefc7272687d39c6102e8ef1e4c4104da1
Jaskarandeep Singh Jashan <jaskarandeepsinghjashan@gmail.com>
2025-12-23 01:34:18 -0600

    started day 3

---
+++ day3.js
@@ -0,0 +1,17 @@
+// creating a to-do list aeeay and using loop to iterate throught the items
+const todos = ["sleep", "eat", "code"];
+for ( let i = 0; i < todos.length; i++){
+    console.log(todos[i]);
+
+}
+
+
+// using enhanced for loop
+for (const todo of todos){
+    console.log(todo);
+}
+
+// functional loop
+todos.forEach((todo) => {
+    console.log(todo);
+});
